6647f785f90df14e82828119ad1d187284073dee
roberto <roberto@roberto-Aspire-5745.(none)>
2016-03-20 23:51:17 -0600

    agregando opciones en javascript

--- public/js/application.js
+++ public/js/application.js
@@ -1,17 +1,36 @@
 $(document).ready(function() {
 	
+  $("#add_option").hide();
+
+  var countQuestion = 1;//contador para los div de las preguntas
+  var lastQuestionDiv = null;
 
   $("#create_question").click(function() {
-    console.log("boton")
+    //console.log("boton")
     var question = $("#name_question").val();
-    console.log(question)
-    $("#questions").append("
-      <div>
-      <h1>"+question+"</h1>
-      
-      </div>");
-    var question = $("#name_question").val("");
+    if(question!="" && question!=null){
+      console.log(question);
+      $("#questions").append(questionDiv(question, countQuestion));
+      countQuestion++;
+      $("#name_question").val("");
+      $("#add_question").hide();
+      $("#add_option").show();
+    }//if
   });
+
+
+  $("#create_option").click(function() {
+    var option = $("#name_question").val();
+    if(option!="" && option!=null){
+      $(lastQuestionDiv).append("");
+    }//if
+  });
+
+
+  function questionDiv(title, number) {
+    lastQuestionDiv = "q"+number;
+    return "<div id=\"q"+number+"\"><h1>"+title+"</h1></div>";
+  }
 
 
 });
